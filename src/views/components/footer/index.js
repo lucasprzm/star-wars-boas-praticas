e802a2a6c9f9ff8f852b895c72c1bb4529dfa854
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-03-19 22:02:50 -0300

    imagem planeta adicionada

---
+++ src/views/components/footer/index.js
@@ -0,0 +1,11 @@
+import planet from "../../../assets/images/elements/planet.png";
+
+let footer = `
+<div>
+  <p>Star wars  -  este é um exemplo criado para os Alunos Gama Academy
+  Módulo de boas práticas.</p>
+  <img src=${planet} alt="Planet" height="180px" width="auto"/>
+</div>
+`;
+
+export default footer;
