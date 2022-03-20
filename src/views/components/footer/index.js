c5e50c871770b6089396c3c5e3781fac3fe1369f
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-03-20 19:02:24 -0300

    classe adicionada para CSS

--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -1,7 +1,7 @@
 import planet from "../../../assets/images/elements/planet.png";
 
 let footer = `
-<div>
+<div class="footer-contain">
   <p>Star wars  -  este é um exemplo criado para os Alunos Gama Academy
   Módulo de boas práticas.</p>
   <img src=${planet} alt="Planet" height="180px" width="auto"/>
